545a08528bf1e59ec07525fefd9f245a77918754
eiiizong <eiiizong@163.com>
2018-11-28 17:49:18 +0800

    新增更改店铺名

---
+++ shandian/static/js/change-info.js
@@ -0,0 +1,17 @@
+
+var app = new Vue({
+	el: '#app',
+	data: function () {
+		return {
+			changText: '川道小炒',
+		}
+	},
+	methods: {
+		init: function () {
+			
+		}
+	},
+	components: {
+		// draggable
+	}
+});
