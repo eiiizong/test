73ee3e241fa8087c231d18bb6b557c8de8c77d57
eiiizong <eiiizong@163.com>
2018-11-29 15:44:43 +0800

    新增添加店员

---
+++ shandian/static/js/add-clerk.js
@@ -0,0 +1,23 @@
+
+var app = new Vue({
+	el: '#app',
+	data: function () {
+		return {
+			foodDesc: '',
+			recommend: true,
+			putaway: false,
+			standard: true,
+			price: '',
+			sizePrice: '',
+			size: ''
+		}
+	},
+	methods: {
+		init: function () {
+			
+		}
+	},
+	components: {
+		// draggable
+	}
+});
