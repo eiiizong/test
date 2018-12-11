db0e8dc4b25202fea11d49672ab8447d29924bfa
eiiizong <eiiizong@163.com>
2018-12-11 17:56:29 +0800

    www

--- shandian/static/js/store-info-change.js
+++ shandian/static/js/store-info-change.js
@@ -2,12 +2,29 @@ var app = new Vue({
 	el: '#app',
 	data: function () {
 		return {
-			checkBoxData: []
+			storeInfo: {
+				headImgUrl: 'http://oupo5zwub.bkt.clouddn.com/blog/170816/10IA3EL4IF.jpg?imageslim',
+				name: '川道小炒',
+				address: '成都市青羊区牧电路23号',
+				merchant: '洋富柜',
+				tel: '18482160080',
+			},
+			text: '',
+			popupVisible: false
 		}
 	},
 	methods: {
 		init: function () {
 			
+		},
+		changeInfo(e) {
+			this.text = e;
+			this.popupVisible = true;
+			console.log(e);
+		},
+		saveInfo() {
+			var val = this.text;
+			this.popupVisible = false;
 		}
 	}
 });
