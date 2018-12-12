4c84598504e991dd7875547e63d15253d88de279
eiiizong <eiiizong@163.com>
2018-12-12 17:34:06 +0800

    菜品分类

--- shandian/static/js/bank-card-management.js
+++ shandian/static/js/bank-card-management.js
@@ -3,11 +3,59 @@ var app = new Vue({
 	el: '#app',
 	data: function () {
 		return {
+			cardList: [
+				{	className: 'CCB',
+					name: '建设银行',
+					isDefault: true,
+					type: '储蓄卡',
+					cardNumber: '**** **** **** **** 479',
+					imgUrl: 'http://oupo5zwub.bkt.clouddn.com/blog/170816/10IA3EL4IF.jpg?imageslim'
+				},
+				{
+					className: 'ICBC',
+					name: '工商银行',
+					isDefault: false,
+					type: '储蓄卡',
+					cardNumber: '**** **** **** **** 479',
+					imgUrl: 'http://oupo5zwub.bkt.clouddn.com/blog/170816/10IA3EL4IF.jpg?imageslim'
+				},
+				{
+					className: 'ICCB',
+					name: '工商银行',
+					isDefault: false,
+					type: '储蓄卡',
+					cardNumber: '**** **** **** **** 479',
+					imgUrl: 'http://oupo5zwub.bkt.clouddn.com/blog/170816/10IA3EL4IF.jpg?imageslim'
+				}
+			],
 		} 
 	},
 	methods: {
 		init: function () {
 			
+		},
+		setDafultCard(index) {
+			console.log(index);
+			var data = this.cardList;
+			for (var i = 0; i < data.length; i++) {
+				data[i].isDefault = false;
+			}
+			data[index].isDefault = true;
+		},
+		deleteCard(index) {
+			this.cardList.splice(index,1);
+			var data = this.cardList;
+			var num = 0;
+			for (var i = 0; i < data.length; i++) {
+				if(data[i].isDefault) {
+					return false;
+				} else {
+					num++;
+					if (num == data.length) {
+						data[0].isDefault = true;
+					}
+				}
+			}
 		}
 	},
 	components: {
--- shandian/static/js/food-type-management.js
+++ shandian/static/js/food-type-management.js
@@ -1,24 +1,113 @@
-
 var app = new Vue({
 	el: '#app',
-	data: function () {
+	data: function() {
 		return {
-			myArray: [
-				{ name: '滋味灌汤', fixed: true, order: 0 },
-				{ name: '口味干锅', fixed: false, order: 1 },
-				{ name: '美味盖饭', fixed: false, order: 2 },
-				{ name: '极品鸭舌', fixed: false, order: 3 },
-				{ name: '有道有味', fixed: false, order: 4 },
-				{ name: '宜宾燃面', fixed: false, order: 5 }
-			]
+			myArray: [{
+					name: '滋味灌汤',
+				},
+				{
+					name: '口味干锅',
+				},
+				{
+					name: '美味盖饭',
+				},
+				{
+					name: '极品鸭舌',
+				},
+				{
+					name: '有道有味',
+				},
+				{
+					name: '宜宾燃面',
+				}
+			],
+			isOrding: false,
+			allNum: '',
+			content: '',
+			isAddFoodInputShow: false,
+			index: '',
+			showPopup: false,
+			changeValue: '',
+			showInputPopup: false,
+			oldTypeName: ''
 		}
 	},
+	mounted() {
+		this.init()
+	},
 	methods: {
-		init: function () {
+		init: function() {
+			this.allNum = this.myArray.length;
+		},
+		ording() {
+			this.isOrding = !this.isOrding;
+		},
+		moveUp(index) {
+			var i = parseInt(index);
+			var arr = this.myArray[i - 1];
+			this.myArray[i - 1] = this.myArray[i];
+			this.myArray[i] = arr;
+			this.$forceUpdate();
+		},
+		moveDown(index) {
+			var i = parseInt(index);
+			console.log(i);
+			var arr = this.myArray[i + 1];
+			this.myArray[i + 1] = this.myArray[i];
+			this.myArray[i] = arr;
+			this.$forceUpdate();
+		},
+		addFood() {
+			this.isAddFoodInputShow = !this.isAddFoodInputShow;
+		},
+		complete() {
+			this.isAddFoodInputShow = !this.isAddFoodInputShow;
+			var text = this.content;
+			if (text) {
+				var obj = {};
+				obj.name = text;
+				this.myArray.push(obj);
+				this.content = '';
+				this.$forceUpdate();
+			}
+			this.allNum = this.myArray.length;
+		},
+		clickShowPopup(index) {
+			console.log(index);
+			this.index = index;
+			this.showPopup = !this.showPopup;
+		},
+		deleteOK() {
+			var tag = this.index;
+			this.myArray.splice(tag, 1);
+			this.$forceUpdate();
+			this.showPopup = !this.showPopup;
+		},
+		cancel() {
+			var tag = this.index;
+			this.showPopup = !this.showPopup;
+		},
+		clickShowInputPopup(index) {
+			this.index = index;
+			this.showInputPopup = !this.showInputPopup;
+			this.oldTypeName = this.myArray[index].name;
 			
+			var DOM = this.$refs.inputDOM;;
+			DOM.focus();
+		},
+		cancelChange() {
+			this.showInputPopup = !this.showInputPopup;
+		},
+		changeOK() {
+			var index = this.index;
+			this.myArray[index].name = this.changeValue;
+			this.showInputPopup = !this.showInputPopup;
+			this.$forceUpdate();
+			this.changeValue = '';
 		}
 	},
 	components: {
 		// draggable
-	}
+	},
+
 });
