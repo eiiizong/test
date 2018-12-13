d31289b8316c89382b8de69e6a2be2c99ead97a1
eiiizong <eiiizong@163.com>
2018-12-13 17:08:49 +0800

    是

---
+++ shandian/static/js/default-category.js
@@ -0,0 +1,17 @@
+
+var app = new Vue({
+	el: '#app',
+	data: function () {
+		return {
+			list: ['中餐馆','面馆','小吃店','快餐店','咖啡店','西餐店']
+		} 
+	},
+	methods: {
+		init: function () {
+			
+		}
+	},
+	components: {
+		// draggable
+	}
+});
---
+++ shandian/static/js/my-app.js
@@ -0,0 +1,17 @@
+
+var app = new Vue({
+	el: '#app',
+	data: function () {
+		return {
+			haveApp: false
+		} 
+	},
+	methods: {
+		init: function () {
+			
+		}
+	},
+	components: {
+		// draggable
+	}
+});
