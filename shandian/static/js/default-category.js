5788d6e53b7c78a4e01be90e484a8d8824f69cd5
eiiizong <eiiizong@163.com>
2019-01-10 18:39:35 +0800

    20190110

--- shandian/static/js/default-category.js
+++ shandian/static/js/default-category.js
@@ -1,14 +1,116 @@
-
 var app = new Vue({
 	el: '#app',
-	data: function () {
+	data: function() {
 		return {
-			list: ['中餐馆','面馆','小吃店','快餐店','咖啡店','西餐店']
-		} 
+			defaultSizes: [{
+					name: '小酒店',
+					children: [{
+							sizeName: '大',
+							checked: false
+						},
+						{
+							sizeName: '中',
+							checked: false
+						},
+						{
+							sizeName: '小',
+							checked: false
+						},
+						{
+							sizeName: '大',
+							checked: false
+						},
+						{
+							sizeName: '中',
+							checked: false
+						},
+						{
+							sizeName: '小',
+							checked: false
+						}
+					],
+					selected: false
+				},
+				{
+					name: '中餐厅',
+					children: [{
+							sizeName: '大',
+							checked: false
+						},
+						{
+							sizeName: '中',
+							checked: false
+						},
+						{
+							sizeName: '小',
+							checked: false
+						}
+					],
+					selected: false
+				},
+				{
+					name: '西餐厅',
+					children: [{
+							sizeName: '大',
+							checked: false
+						},
+						{
+							sizeName: '中',
+							checked: false
+						},
+						{
+							sizeName: '小',
+							checked: false
+						}
+					],
+					selected: false
+				}
+			],
+			index: 0
+		}
 	},
 	methods: {
-		init: function () {
-			
+		init: function() {
+
+		},
+		allChoice() {
+			var data = this.defaultSizes[this.index].children;
+			data.map(function(item, i) {
+				data[i].checked = true;
+			});
+		},
+		sizeChange(index) {
+			console.log(index);
+			var data = this.defaultSizes[this.index].children;
+			data.map((item, i) => {
+				if (i == index) {
+					data[index].checked = !data[index].checked;
+				}
+			});
+		},
+		choiceMainType(index) {
+			this.index = index;
+			var data = this.defaultSizes;
+			data.map(function(item, i) {
+				if (i == index) {
+					data[i].selected = true;
+				} else {
+					data[i].selected = false;
+				}
+			});
+			this.allChoice();
+		},
+		save() {
+			var data = this.defaultSizes[this.index].children;
+			var type = this.defaultSizes[this.index].name;
+			console.log('您选择的餐厅类型是：', type);
+			var arr = [];
+			data.map(function(item, i) {
+				if (item.checked) {
+					arr.push(data[i].sizeName);
+				}
+			});
+			console.log('您选择的餐厅规格有：', arr.toString());
 		}
 	},
 	components: {
--- shandian/static/js/gain-recording.js
+++ shandian/static/js/gain-recording.js
@@ -1,119 +1,116 @@
-
 var app = new Vue({
 	el: '#app',
-	data: function () {
+	data: function() {
 		return {
-			orderList: [
-				{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 236,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: true,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},{
-					imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
-					orderNum: 239,
-					orderPeople: 'Eiiizong',
-					orderTime: '今天  12:34',
-					isHelpOrder: false,
-					foodPrice: '11.15'
-				},
-			],
+			orderList: [{
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 236,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: true,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, {
+				imgUrl: 'https://avatars1.githubusercontent.com/u/30979753?s=400&u=1de5d828ea2597ca7618410665fcf23580c8f892&v=4',
+				orderNum: 239,
+				orderPeople: 'Eiiizong',
+				orderTime: '今天  12:34',
+				isHelpOrder: false,
+				foodPrice: '11.15'
+			}, ],
 			deta: '',
 			totalSale: '854453.22',
 			pickerValue: new Date()
-		} 
+		}
 	},
-	mounted () {
+	mounted() {
 		this.init();
 	},
 	methods: {
-		init: function () {
-			this.initDate();
+		init: function() {
+			this.calcDate(new Date());
 		},
-		getTotalMoney: function () {
+		getTotalMoney: function() {
 			var _this = this;
 			var data = _this.orderList;
 			var len = data.length;
 		},
-		initDate: function() {
-			var now = new Date();
-			var years = now.getFullYear();
-			var month = now.getMonth() + 1;
-			this.deta = years + '-' + month
+		// 计算时间 格式化时间
+		calcDate: function(date) {
+			var years = date.getFullYear();
+			var month = date.getMonth() + 1;
+			if (month > 0 && month < 10) {
+				month = '0' + month;
+			}
+			var day = date.getDate();
+			if (day > 0 && day < 10) {
+				day = '0' + day;
+			}
+			this.deta = years + '-' + month + '-' + day;
 		},
 		openPicker: function() {
-			var rootDOM = this.$el;
-			var targetDOM = rootDOM.getElementsByClassName('picker-slot picker-slot-center');
-			targetDOM[targetDOM.length - 1].style.display = 'none';
-			console.log(targetDOM);
-			
 			this.$refs.picker.open();
 		},
 		handleConfirm: function(e) {
-			var years = e.getFullYear();
-			var month = e.getMonth() + 1;
-			this.deta = years + '-' + month;
+			this.calcDate(e);
 		},
 		cancal: function() {
-			
+
 		}
 
 	},
