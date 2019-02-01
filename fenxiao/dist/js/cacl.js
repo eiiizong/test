1fd19056b2b1bb81b6e34e4ce4798e7f33527180
eiiizong <eiiizong@163.com>
2019-02-01 11:09:38 +0800

    test01

--- fenxiao/dist/js/cacl.js
+++ fenxiao/dist/js/cacl.js
@@ -1,11 +1,14 @@
 // 计算提交审核页面li标签的宽度
-var cacl = function () {
-	var ul = document.getElementById('img_warpper');
-	var lis = ul.getElementsByClassName('left');
-	var w = lis[0].offsetWidth;
-	console.log(w);
-	for(let i = 0; i<lis.length; i++) {
-		lis[i].style.height = w + 'px';
-	}
+window.onload = function () {
+	var cacl = function () {
+		var ul = document.getElementById('img_warpper');
+		var lis = ul.getElementsByClassName('left');
+		var w = lis[0].offsetWidth;
+		alert(w);
+		console.log(w);
+		for(let i = 0; i<lis.length; i++) {
+			lis[i].style.height = w + 'px';
+		}
+	};
+	cacl();
 };
-cacl()
