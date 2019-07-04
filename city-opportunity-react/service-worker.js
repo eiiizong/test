3ca708f7236651c44b3079d50944595de27d5bd3
eiiizong <eiiizong@163.com>
2019-07-04 11:17:08 +0800

    react-city

--- city-opportunity-react/service-worker.js
+++ city-opportunity-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b70e71fdbc795f1cb65879a846c1078.js"
+  "./precache-manifest.387e22596e12a02eebc3b5ef6c6d76b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
